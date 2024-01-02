b50bff11e2c60790c908f3b57fd53e57198440ad
Luiz Oliveira <luizfpeoliveira@gmail.com>
2024-01-02 18:14:05 -0300

    Initial Commit

---
+++ TakeHomeApp/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  TakeHomeApp
+//
+//  Created by Luiz Oliveira on 02/01/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
