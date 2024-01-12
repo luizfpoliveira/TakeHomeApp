3a152f343cc53a4fb0bfaad823652d522f796fe6
luiz Oliveira <luizfpeoliveira@gmail.com>
2024-01-12 12:53:40 -0300

    Refactored network code

---
+++ TakeHomeApp/Managers/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  TakeHomeApp
+//
+//  Created by Luiz Oliveira on 11/01/24.
+//
+
+import Foundation
---
+++ TakeHomeApp/Utils/ErrorMessage.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorMessage.swift
+//  TakeHomeApp
+//
+//  Created by Luiz Oliveira on 12/01/24.
+//
+
+import Foundation
